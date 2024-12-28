4727a46366b3491b3299149a526da82940a9c7a1
kumre005 <kumre005@gmail.com>
2024-12-28 21:09:58 +0530

    Main python file

---
+++ main.py
@@ -0,0 +1,20 @@
+# pip install flask ovencv-python
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hellow_world():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+app.run(debug = True, port = 5000)
