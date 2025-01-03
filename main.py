ac4fba2950b0cbe07226e4b17a47ab7670f14a3f
kumre005 <kumre005@gmail.com>
2025-01-03 16:15:18 +0530

    create readme, modify main and index

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # pip install flask ovencv-python
 
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -16,6 +16,12 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/edit", methods = ['POST', 'GET'])
+def edit():
+    if request.method == 'POST':
+        return "POST request is here...!"
+    return render_template("index.html")
 
 app.run(debug = True, port = 5000)
 
