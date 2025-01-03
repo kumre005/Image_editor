541588a380a7d455917aa3cf382ba661ed21e6ff
kumre005 <kumre005@gmail.com>
2025-01-03 17:41:37 +0530

    uploading photos, changes in index, adding somem function

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # pip install flask ovencv-python
 
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, flash
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'webp', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def home():
@@ -20,7 +30,22 @@ def contact():
 @app.route("/edit", methods = ['POST', 'GET'])
 def edit():
     if request.method == 'POST':
-        return "POST request is here...!"
+          # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return "error"
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return "redirect(request.url)"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #procees_image()
+            return render_template("index.html")
+
     return render_template("index.html")
 
 app.run(debug = True, port = 5000)
